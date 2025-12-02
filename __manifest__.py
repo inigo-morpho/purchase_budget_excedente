26eab1449d83ae40319201bf8626f1d29c63c4f9
inigo-morpho <inigo.e@morphology.com.mx>
2025-12-01 18:51:36 -0600

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Purchase Excedente',
+    'version': '1.1',
+    'category': 'Purchases',
+    'summary': 'Calcula excedente por línea y total en órdenes de compra',
+    'author': 'Tu Nombre',
+    'depends': ['purchase'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
